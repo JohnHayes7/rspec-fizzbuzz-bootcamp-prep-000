1a88fd4168ad7f36e5005d04b01e62a27b96a7cb
John Hayes <johnnyhayes7@gmail.com>
2019-01-14 15:52:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 1 
+  if int % 3 == 0 
     "Fizz"
   end
   
-  if int % 5 == 1
+  if int % 5 == 0
     "Buzz"
   end
   
